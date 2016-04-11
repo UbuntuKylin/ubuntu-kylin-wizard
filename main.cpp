5004675d5928fa8a9a79478fe5d4e5c321ecdd39
handsome_feng <445865575@qq.com>
2016-04-11 23:57:41 +0800

    Makes program start after unity-launcher

--- main.cpp
+++ main.cpp
@@ -6,27 +6,43 @@
 #include <gdk/gdkx.h>
 #include <X11/Xlib.h>
 #include <unistd.h>
-#include <iostream>
 
-int main (int argc, char *argv[])
+
+gboolean wait_launcher()
 {
-  gtk_init(&argc, &argv);
-
   GdkScreen *screen = gdk_screen_get_default();
   GdkDisplay *display = gdk_screen_get_display(screen);
   GList* stack =  gdk_screen_get_window_stack(screen);
+
+  gboolean found = false;
   for( GList* iter = g_list_last(stack); iter; iter = iter->prev)
   {
     GdkWindow* window = static_cast<GdkWindow*>(iter->data);
     XID xid = GDK_WINDOW_XID(window);
     char *name;
     XFetchName(GDK_DISPLAY_XDISPLAY(display), xid, &name);
-    std::cout << name << std::endl;
+    if (g_strcmp0(name, "unity-launcher") == 0)
+    {
+      found = true;
+      break;
+    }
   }
   g_list_foreach(stack, (GFunc)g_object_unref, NULL);
   g_list_free(stack);
+  sleep(1);
+  return found;
+}
 
-  sleep(5);
+int main (int argc, char *argv[])
+{
+  gtk_init(&argc, &argv);
+
+  gboolean launcher_showed = false;
+  while(!launcher_showed)
+  {
+    launcher_showed = wait_launcher();
+  }
+
   Draw *draw = new Draw();
   draw->Run();
 
