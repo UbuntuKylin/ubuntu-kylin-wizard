8c3150f018d1a3d6d2e883fb97b1eda336836b2b
Aylinux <cihanalk@gmail.com>
2020-07-19 19:20:09 +0300

    Create tr.ts

---
+++ po/tr.ts
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr_TR" sourcelanguage="en">
+<context>
+    <name>AppModel</name>
+    <message>
+        <location filename="../src/appmodel.cpp" line="85"/>
+        <source>%1
+%2</source>
+        <translation>%1
+%2</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>Ana Pencere</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="45"/>
+        <source>Here you can install the following third-party applications from &quot;kylin software center&quot;:</source>
+        <translation>Burada, &quot;kylin yazılım merkezi&quot; nden aşağıdaki üçüncü taraf uygulamaları yükleyebilirsiniz:</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="57"/>
+        <source>Go to the &quot;kylin software center&quot; later to see more apps</source>
+        <translation>Daha fazla uygulama görmek için daha sonra &quot;kylin yazılım merkezine&quot; gidin</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.ui" line="67"/>
+        <source>Install</source>
+        <translation>Kur</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="112"/>
+        <source>Install %1 success!
+</source>
+        <translation>%1 yükleme başarılı!
+</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="114"/>
+        <source>Install %1 failed!
+</source>
+        <translation>%1 yükleme başarısız!
+</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="120"/>
+        <source>Error when install software, please open ubuntu kylin software center to install them manally!</source>
+        <translation>Yazılım yüklenirken hata oluştu, lütfen manuel olarak kurmak için ubuntu kylin yazılım merkezini açın!</translation>
+    </message>
+</context>
+<context>
+    <name>QMessageBox</name>
+    <message>
+        <location filename="../src/main.cpp" line="76"/>
+        <source>You are offline, please connect to the network first and then press &apos;OK&apos; to run this wizard, or press &apos;Cancel&apos; to quit.</source>
+        <translation>Çevrimdışı durumdasınız, lütfen önce ağa bağlanın ve ardından bu sihirbazı çalıştırmak için &apos;Tamam&apos;a basın veya çıkmak için&apos; İptal&apos;e basın.</translation>
+    </message>
+</context>
+</TS>
